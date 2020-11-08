b90613191e6dabf9258f5167ec023c20e3c7fa1f
LeeJun <alex.leejun@zrp.com.br>
2020-11-08 18:50:13 -0300

    api login tests created

--- api/auth.py
+++ api/auth.py
@@ -3,6 +3,7 @@ from flask import (
     request,
 )
 import json
+from adapters.database_config import get_session
 from services import auth_services
 
 bp = Blueprint("auth", __name__, url_prefix="/auth")
@@ -10,17 +11,19 @@ bp = Blueprint("auth", __name__, url_prefix="/auth")
 
 @bp.route("/register", methods=["POST"])
 def register_route():
+    session = get_session()
     try:
-        user = auth_services.create_user(**dict(request.json))
-        return json.dumps(user), 200
+        user = auth_services.create_user(session, **dict(request.json))
+        return user, 201
     except:
         return "Não foi possível criar usuario", 400
 
 
 @bp.route("/login", methods=["POST"])
 def login_route():
+    session = get_session()
     try:
-        response = auth_services.login(**dict(request.json))
-        return json.dumps(response), 200
+        response = auth_services.login(session, **dict(request.json))
+        return response, 200
     except:
         return "Acesso negado!", 400
---
+++ tests/integration/api/test_auth.py
@@ -0,0 +1,107 @@
+import pytest
+
+
+@pytest.fixture(name="valid_data")
+def valid_data_fixture():
+    return {
+        "name": "Marcio Ribeiro",
+        "email": "marcio_ribeiro@hotmail.com",
+        "password": "Ysd24@21f",
+    }
+
+
+@pytest.fixture(name="invalid_data")
+def invalid_data_fixture():
+    return {"name": "", "email": "email.email.com", "password": "password123"}
+
+
+class TestRegister:
+    endpoint = "/auth/register"
+
+    def test_register_with_valid_data(self, app, valid_data):
+        with app.test_client() as client:
+            response = client.post(self.endpoint, json=valid_data)
+
+        body = response.get_json()
+        assert body["token"] is not None
+        assert response.status_code == 201
+
+    def test_register_with_invalid_name(self, app, valid_data, invalid_data):
+        with app.test_client() as client:
+            response = client.post(
+                self.endpoint,
+                json={**valid_data, "name": invalid_data["name"]},
+            )
+
+        body = response.get_json()
+        assert body is None
+        assert response.status_code == 400
+
+    def test_register_with_invalid_email(self, app, valid_data, invalid_data):
+        with app.test_client() as client:
+            response = client.post(
+                self.endpoint,
+                json={**valid_data, "email": invalid_data["email"]},
+            )
+
+        body = response.get_json()
+        assert body is None
+        assert response.status_code == 400
+
+    def test_register_with_invalid_password(self, app, valid_data, invalid_data):
+        with app.test_client() as client:
+            response = client.post(
+                self.endpoint,
+                json={
+                    **valid_data,
+                    "password": invalid_data["password"],
+                },
+            )
+
+        body = response.get_json()
+        assert body is None
+        assert response.status_code == 400
+
+
+class TestLogin:
+    endpoint = "/auth/login"
+
+    def test_login_with_valid_data(self, app, valid_data):
+        with app.test_client() as client:
+            client.post("/auth/register", json=valid_data)
+
+        with app.test_client() as client:
+            response = client.post(
+                self.endpoint,
+                json=valid_data,
+            )
+
+        body = response.get_json()
+        assert body["token"] is not None
+        assert response.status_code == 200
+
+    def test_login_with_wrong_email(self, app, valid_data, invalid_data):
+        with app.test_client() as client:
+            client.post("/auth/register", json=valid_data)
+
+        with app.test_client() as client:
+            response = client.post(
+                self.endpoint,
+                json={**valid_data, "email": "mercio@hotmail.com"},
+            )
+
+        assert response.data.decode("utf-8") == "Acesso negado!"
+        assert response.status_code == 400
+    
+    def test_login_with_wrong_password(self, app, valid_data, invalid_data):
+        with app.test_client() as client:
+            client.post("/auth/register", json=valid_data)
+
+        with app.test_client() as client:
+            response = client.post(
+                self.endpoint,
+                json={**valid_data, "password": "Marc12@15"},
+            )
+
+        assert response.data.decode("utf-8") == "Acesso negado!"
+        assert response.status_code == 400
